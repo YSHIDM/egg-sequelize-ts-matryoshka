164b09978563fc9ae4a805e7c2dcbba382862a78
YSHIDM <1179082420@qq.com>
2022-03-22 11:37:23 +0800

    合并

--- app/service/LogsSvc.ts
+++ app/service/LogsSvc.ts
@@ -2,48 +2,63 @@ import { Service } from 'egg';
 // import { Sequelize } from 'sequelize-typescript';
 
 export default class LogsSvc extends Service {
-
-    public async getLogs(id) {
-        const model = this.ctx.model.Logs;
-        const logs = model.findOne({
-            where: {
-                id
-            }
-        }).then(d => d?.toJSON());
-        return logs;
-    }
-    public async addLogs(obj) {
+    public async addLogs(obj, transaction?) {
+        let options = {};
+        if (transaction) {
+            options = { transaction }
+        }
         const { ctx, app } = this;
-        const model = ctx.model.Logs;
+        const model = ctx.model.Log;
         obj['id'] = app['genId']('IMG')
-        const logs = await model.create(obj).then(d => d?.toJSON());
-        return logs;
+        return await model.create(obj, options).then(d => d?.toJSON());
     }
-    public async updateLogs(obj) {
-        const model = this.ctx.model.Logs;
+    public async updateLogs(obj, transaction?) {
+        const model = this.ctx.model.Log;
         await model.update(obj, {
             where: {
-                id: obj.id
-            }
+                id: obj.id,
+            },
+            transaction,
         });
-        return model.findByPk(obj.id).then(d=>d?.toJSON());
+        return await this.byPk(obj.id);
     }
-    public async deleteLogs(id) {
-        const model = this.ctx.model.Logs;
-        const logs = model.destroy({
-            where: {
-                id
-            }
+    /**
+     * 按id查询日志
+     * @param id 主键
+     */
+    public async byPk(id) {
+        const model = this.ctx.model.Log;
+        return await model.findByPk(id).then(d => d?.toJSON());
+    }
+    /**
+     * 获取一条日志
+     * @param where 条件
+     */
+    public async getLogs(where) {
+        where = where?.where ?? where
+        const model = this.ctx.model.Log;
+        return await model.findOne({ where }).then(d => d?.toJSON());
+    }
+    /**
+     * 获取日志列表
+     * @param where 条件
+     */
+    public async getLogsList(where) {
+        where = where?.where ?? where
+        const model = this.ctx.model.Log;
+        return await model.findAll({ where }).then(ds => ds.map(d => d.toJSON()));
+    }
+    /**
+     * 删除日志
+     * @param where 条件
+     * @param transaction 事务
+     */
+    public async deleteLogs(where, transaction?) {
+        where = where?.where ?? where
+        const model = this.ctx.model.Log;
+        return await model.destroy({
+            where,
+            transaction,
         });
-        return logs;
-    }
-    public async find(obj) {
-        const model = this.ctx.model.Logs;
-        const logs = model.findAll({
-            where: {
-                id:obj.ids
-            }
-        }).then(ds=>ds.map(d=>d.toJSON()));
-        return logs;
     }
 }
--- test/app/service/logs.test.ts
+++ test/app/service/logs.test.ts
@@ -23,76 +23,47 @@ describe('get()', () => {
     console.log('get', logs)
     // assert(logs);
   })
+  it('test ids', async () => {
+    // 创建 ctx
+    const ctx = app.mockContext();
+    // 通过 ctx 访问到 service.user
+    const logs = await ctx.service.logsSvc.find({
+      ids: ['IMGhek0eLOq', 'IMG8VU-zgfr']
+    })
+    console.log('update', logs)
+    // assert(logs);
+  });
+  // it('get', async () => {
+  //     // 创建 ctx
+  //     const ctx = app.mockContext();
+  //     // 通过 ctx 访问到 service.user
+  //     const logs = await ctx.service.logsSvc.getLogs('IMGhek0eLOq')
+  //     console.log('get', logs)
+  //     assert(logs);
+  // });
   // it('add', async () => {
   //     // 创建 ctx
   //     const ctx = app.mockContext();
   //     // 通过 ctx 访问到 service.user
-  //     console.log('ctx', ctx)
-  //     const logs = await ctx.service.logSvc.addLogs({
+  //     const logs = await ctx.service.logsSvc.addLogs({
   //         title: 'add',
   //         content: 'add logs',
   //         like: 1
   //     })
   //     console.log('add', logs)
-  //     // assert(logs);
+  //     assert(logs);
   // });
   // it('update', async () => {
   //     // 创建 ctx
   //     const ctx = app.mockContext();
   //     // 通过 ctx 访问到 service.user
-  //     const logs = await ctx.service.logSvc.updateLogs({
+  //     const logs = await ctx.service.logsSvc.updateLogs({
   //         id: 'IMGhek0eLOq',
   //         title: 'update',
   //         content: 'update logs',
   //         like: 1
   //     })
   //     console.log('update', logs)
-  //     // assert(logs);
+  //     assert(logs);
   // });
 })
-    it('test ids', async () => {
-        // 创建 ctx
-        const ctx = app.mockContext();
-        // 通过 ctx 访问到 service.user
-        const logs = await ctx.service.logsSvc.find({
-            ids: ['IMGhek0eLOq','IMG8VU-zgfr']
-        })
-        console.log('update', logs)
-        // assert(logs);
-    });
-    // it('get', async () => {
-    //     // 创建 ctx
-    //     const ctx = app.mockContext();
-    //     // 通过 ctx 访问到 service.user
-    //     const logs = await ctx.service.logsSvc.getLogs('IMGhek0eLOq')
-    //     console.log('get', logs)
-    //     assert(logs);
-    // });
-    // it('add', async () => {
-    //     // 创建 ctx
-    //     const ctx = app.mockContext();
-    //     // 通过 ctx 访问到 service.user
-    //     const logs = await ctx.service.logsSvc.addLogs({
-    //         title: 'add',
-    //         content: 'add logs',
-    //         like: 1
-    //     })
-    //     console.log('add', logs)
-    //     assert(logs);
-    // });
-    // it('update', async () => {
-    //     // 创建 ctx
-    //     const ctx = app.mockContext();
-    //     // 通过 ctx 访问到 service.user
-    //     const logs = await ctx.service.logsSvc.updateLogs({
-    //         id: 'IMGhek0eLOq',
-    //         title: 'update',
-    //         content: 'update logs',
-    //         like: 1
-    //     })
-    //     console.log('update', logs)
-    //     assert(logs);
-    // });
-
-
-});
